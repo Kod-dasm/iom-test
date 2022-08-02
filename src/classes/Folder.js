aa6fa28e96341ead8d1ba1c7a84fe54fc1616e02
Your Name <you@example.com>
2022-08-02 13:23:03 +0500

    add functionality

--- src/classes/Folder.js
+++ src/classes/Folder.js
@@ -1,10 +1,13 @@
 import { Node } from ".";
 
 export default class Folder extends Node {
+  number = 0;
   icon = "../assets/folder.png";
-  children = [];
+  childrens = [];
 
-  add() {
-    
+  add(node) {
+    this.childrens.push(node);
   }
+
+  getNewId() { return `${this.id}-${++this.number}` }
 }
--- src/classes/Node.js
+++ src/classes/Node.js
@@ -1,10 +1,32 @@
 export default class Node {
-  constructor(number, name = this.constructor.name) {
-    this.id = number + 1;
+  constructor(id, name = this.constructor.name) {
+    this.id = id;
     this.name = `${name} ${this.id}`;
   }
 
-  remove() {
+  sayHi() {
+    console.log(this);
+  }
 
+  remove(arrayNodes) {
+    return this.findNodeToRemove(arrayNodes, this.id);
+  }
+
+  findNodeToRemove(nodes, id) {
+    for (let node of nodes) {
+      const idCurrentDepth = this.getIdCurrentDepth(node.id);
+      if (String(node.id) === idCurrentDepth) {
+        if (idCurrentDepth === String(id)) {
+          return nodes.filter((item) => item.id !== id);
+        }
+        node.childrens = this.findNodeToRemove(node.childrens, id);
+        return nodes;
+      }
+    }
+  }
+
+  getIdCurrentDepth(id) {
+    const lengthId = String(this.id).split("-").length;
+    return String(id).split("-").slice(0, lengthId).join("-");
   }
 }
